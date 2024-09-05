675c987e6f92b81fa0036579a52a8540d7c12ead
nickinho0 <nickolasleal6@gmail.com>
2024-09-05 11:40:50 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+class
+
+
+
+
+
+
+
+
+
+#Exercício de Python: Classe Animais de Estimação
+#Objetivo: Criar uma classe em Python para representar animais de estimação, com os métodos:
+#Requisitos:
+#A classe deve se chamar AnimalEstimacao.
+#A classe deve ter os seguintes atributos:
+#nome: nome do animal
+#especie: espécie do animal (ex: cachorro, gato, peixe)
+#idade: idade do animal em anos
+#estado: estado que o animal se encontra (´Comendo', 'Dormindo','Brincando')
+#A classe deve ter os seguintes métodos:
+#__init__(self, nome, especie, idade): método construtor da classe, que inicializa os atributos
+#comer(): método que simula o animal comendo
+#dormir(): método que simula o animal dormindo
+#brincar(): método que simula o animal brincando
+#listar(): que retorna uma string com a representação textual do animal (ex: "Nome: Rex, Espécie: Cachorro, Idade: 3 anos,  Estado:Comendo").
+#Exemplo de uso
+
+#animal = AnimalEstimacao("Rex", "Cachorro", 3) animal.comer() animal.dormir() animal.brincar() animal.listar()
+
+#Desafio:
+#Adicione mais métodos à classe para representar outros comportamentos dos animais de estimação (ex: latir, miar, nadar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
