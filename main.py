31da2b07158f58316e0b76ee4dadfd8f67e773cb
nickinho0 <nickolasleal6@gmail.com>
2024-09-07 15:06:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,55 @@
-class
+class AnimalEstimacao:
+    def __init__(self, nome, especie, idade):
+     self.nome = nome
+     self.especie = especie
+     self.idade = idade
+     self.estado = 'Dormindo'
+
+    def comer(self):
+     self.estado = 'Comendo'
+     print(f"O {self.nome} está comendo!")
+
+    def dormir(self):
+     self.estado = 'Dormindo'
+     print(f"O {self.nome} continua dormindo!")
+
+    def brincar(self):
+     self.estado = 'Brincando'
+     print(f"O {self.nome} está brincando!")
+
+    def listar(self):
+     return (f"Nome: {self.nome}, Espécie: {self.especie}, Idade: {self.idade}, Estado: {self.estado}")
+
+    def latir(self):
+      if self.especie.lower == 'cachorro':
+       self.estado = 'Latindo'
+       print(f"O {self.especie} chamado {self.nome} está latindo!")
+      else:
+        print("Ops!,você digitou errado,gatos não latem!")
+
+    def miar(self):
+      if self.especie.lower == 'gato':
+       self.estado = 'Miando'
+       print(f"O {self.especie} chamado {self.nome} está miando!")
+      else:
+          print("Ops!,você digitou errado,cachorros não miam!")
+
+    def nadar(self):
+      if self.especie.lower == 'peixe':
+       self.estado = 'Nadando'
+       print(f"O {self.especie} chamado {self.nome} está nadando!")
+      else:
+          print("Ops!,você digitou errado,cachorros e gatos não respiram embaixo d´água!")
 
 
 
+animal1 = AnimalEstimacao('Duque', 'cachorro', 5)
+animal2 = AnimalEstimacao('Théo', 'gato', 3)
+animal3 = AnimalEstimacao('Garry', 'peixe', 2)
 
-
-
-
-
-
-#Exercício de Python: Classe Animais de Estimação
-#Objetivo: Criar uma classe em Python para representar animais de estimação, com os métodos:
-#Requisitos:
-#A classe deve se chamar AnimalEstimacao.
-#A classe deve ter os seguintes atributos:
-#nome: nome do animal
-#especie: espécie do animal (ex: cachorro, gato, peixe)
-#idade: idade do animal em anos
-#estado: estado que o animal se encontra (´Comendo', 'Dormindo','Brincando')
-#A classe deve ter os seguintes métodos:
-#__init__(self, nome, especie, idade): método construtor da classe, que inicializa os atributos
-#comer(): método que simula o animal comendo
-#dormir(): método que simula o animal dormindo
-#brincar(): método que simula o animal brincando
-#listar(): que retorna uma string com a representação textual do animal (ex: "Nome: Rex, Espécie: Cachorro, Idade: 3 anos,  Estado:Comendo").
-#Exemplo de uso
-
-#animal = AnimalEstimacao("Rex", "Cachorro", 3) animal.comer() animal.dormir() animal.brincar() animal.listar()
-
-#Desafio:
-#Adicione mais métodos à classe para representar outros comportamentos dos animais de estimação (ex: latir, miar, nadar)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(animal1.listar())
+print(animal2.listar())
+print(animal3.listar())
 
 
 
