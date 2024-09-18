7858ab956498ceee0d0bd525ca656bff7f27699f
nickinho0 <nickolasleal6@gmail.com>
2024-09-18 19:27:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -52,6 +52,66 @@ print(animal2.listar())
 print(animal3.listar())
 
 
+#####################################################################################################
 
+
+MOSTRAR TRABALHO SOR:
+class Televisao:
+    def __init__(self, marca, modeloTV):
+     self.marca = marca
+     self.modeloTV = modeloTV
+     self.canal = None
+     self.estado = 'Desligada'  # Estado inicial da tv está desligada
+     self.volume = 0
+
+    def ligar(self):
+     self.estado = 'Ligada'
+     print(f"A {self.marca} {self.modeloTV} está ligada")
+
+    def desligar(self):
+     self.estado = 'Desligada'
+     print(f"A {self.marca} {self.modeloTV} está desligada")
+
+
+    def aumentar_volume(self):
+     if self.estado == 'Desligada':
+      print("Você não pode aumentar o volume da tv porque ela está desligada")
+     else:
+      self.volume += 1
+      print("Você aumentou +1 o volume da tv")
+
+
+    def diminuir_volume(self):
+     if self.estado == 'Desligada':
+      print("Você não pode diminuir o volume da tv porque ela está desligada")
+     else:
+      self.volume -= 1
+      print("Você diminuiu -1 o volume da tv")
+
+    def definir_canal(self, canais):
+        self.canal = canais
+
+    def mostrar_informacoes(self):
+         info = f'Marca: {self.marca}, Modelo: {self.modeloTV}, Canal Atual: {self.canal.canal}'
+
+         print(info)
+
+
+
+class Canais:
+    def __init__(self, canalatual):
+        self.canal = canalatual
+
+
+tv1=Televisao('TCL', 'Smart TV 43 Full HD LED')
+canais1=Canais('RBS TV')
+canais2=Canais('Cazé TV')
+canais3=Canais('Sportv')
+canais4=Canais('Premiere')
+
+tv1.definir_canal(canais1)
+tv1.mostrar_informacoes()
+tv1.definir_canal(canais4)
+tv1.mostrar_informacoes()
 
 
